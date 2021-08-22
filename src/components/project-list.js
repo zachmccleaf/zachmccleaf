d9a2269789217dcd71d5805fc1f7353e85a81df6
Zach McCleaf <anton@Zachs-MacBook-Air.local>
2021-08-22 09:58:57 -0400

    removing  garphql query

--- src/components/project-list.js
+++ src/components/project-list.js
@@ -12,21 +12,3 @@ const ProjectList = () => {
 }
 
 export default ProjectList
-
-export const pageQuery = graphql`
-  query {
-    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
-      edges {
-        node {
-          id
-          excerpt(pruneLength: 250)
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            path
-            title
-          }
-        }
-      }
-    }
-  }
-`
